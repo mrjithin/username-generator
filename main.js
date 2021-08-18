866e74d5a3e5ff7d6558257c01b79d8252e99366
Jithin <silverflames007@gmail.com>
2021-08-18 17:16:11 +0530

    change some mistakes

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const keywords = require('./keywords');
 function generator() {
     'use strict';
     const prompt = require('prompt-sync')({sigint:true});
+    const keywords = require('./keywords');
     console.log('██╗░░░██╗░██████╗███████╗██████╗░███╗░░██╗░█████╗░███╗░░░███╗███████╗░░░░░░░██████╗░███████╗███╗░░██╗███████╗██████╗░░█████╗░████████╗░█████╗░██████╗░');
     console.log('██║░░░██║██╔════╝██╔════╝██╔══██╗████╗░██║██╔══██╗████╗░████║██╔════╝░░░░░░██╔════╝░██╔════╝████╗░██║██╔════╝██╔══██╗██╔══██╗╚══██╔══╝██╔══██╗██╔══██╗');
     console.log('██║░░░██║╚█████╗░█████╗░░██████╔╝██╔██╗██║███████║██╔████╔██║█████╗░░█████╗██║░░██╗░█████╗░░██╔██╗██║█████╗░░██████╔╝███████║░░░██║░░░██║░░██║██████╔╝');
@@ -10,12 +10,9 @@ function generator() {
     console.log('░╚═════╝░╚═════╝░╚══════╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝░░╚═╝╚═╝░░░░░╚═╝╚══════╝░░░░░░░╚═════╝░╚══════╝╚═╝░░╚══╝╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░░╚════╝░╚═╝░░╚═╝');
     console.log('\n\n\nWelcome to my username-generator\nPlease answer all the questions for meaningful results...\n(Press Ctrl+C any time to exit.)');
     console.log('What is your gender? male/female/neutral(m/f/n)');
-    // Takes in input.
-    const genderInput = prompt('Gender: ');
-    // Converts to lower-case.
-    const gender = genderInput.toLowerCase();
-    const frontOrBack = prompt('Do you want your input be added to the front ot back? f/b ');
-    if(frontOrBack.toLowerCase() === 'b'){
+    const gender = prompt('Gender: ').toLowerCase();
+    const frontOrBack = prompt('Do you want your input be added to the front ot back? f/b ').toLowerCase();
+    if(frontOrBack === 'b'){
         if(gender === 'n' || gender === 'neutral' || !gender){
      
         }
